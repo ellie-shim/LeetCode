22d230e1b2d8fbd789f1495e790e829619a9d271
Soyoung Shim <42929997+ellie-shim@users.noreply.github.com>
2026-04-21 20:14:09 +0900

    Time: 4 ms (35.77%), Space: 66.9 MB (7.54%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+var maxProfit = function(prices) {
+  let minPrice = Infinity;
+  let maxProfit = 0;
+
+  for (let price of prices) {
+    minPrice = Math.min(minPrice, price);
+    maxProfit = Math.max(maxProfit, price - minPrice);
+  }
+
+  return maxProfit;
+};
+maxProfit([7,1,5,3,6,4])
