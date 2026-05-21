4997babb18ba91a98aa560b1d7fa414329a28cea
Soyoung Shim <42929997+sotories@users.noreply.github.com>
2026-05-21 17:32:09 +0900

    Time: 47 ms (26.97%), Space: 51.9 MB (98.87%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -2,7 +2,7 @@
  * @return {null|boolean|number|string|Array|Object}
  */
 Array.prototype.last = function () {
-    if (this.length > 0) return this.pop()
+    if (this.length > 0) return this[this.length - 1]
 
     return -1
 };
